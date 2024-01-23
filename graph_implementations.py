edb8634022c0e3b6d2ceb4ac9d1f94baf82e9b45
Juan Bello <jbnermal3@gmail.com>
2024-01-23 18:37:31 -0500

    modularity

--- graph_implementations.py
+++ graph_implementations.py
@@ -12,16 +12,24 @@ class GraphAdjacencyList():
     E = {
         v:[(u,w(v,u)) forall u in N(v)]
         forall v in V
+
     }
     '''
-    def __init__(self, edges):
+    def __init__(self, edges, top_order):
+        '''
+        top_order is topological_order
+        '''
         self.edges = edges
+        self.order = top_order
         
     def get_nodes(self):
         return [key for key in self.edges.keys()]
     
     def get_nbrs(self, v):
         return self.edges[v]
+    
+    def get_order(self):
+        return self.order
     
     def __str__(self):
         graph = ""
